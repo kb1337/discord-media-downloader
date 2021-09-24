ec693eadf099da06ebaaba3251f5ba4e372fc72f
Kadir Burak Gündüz <kburakgndz@gmail.com>
2021-09-24 21:05:54 +0300

    Fixed multiple command conflict and several improvements

--- bot.py
+++ bot.py
@@ -17,13 +17,11 @@ video_regex = "https:\/\/cdn.discordapp.com\/attachments\/[0-9]*\/[0-9]*\/([a-z0
 
 
 def isImage(content):
-    result = re.match(image_regex, str(content).lower())
-    return result.group(2) if result else None
+    return True if re.match(image_regex, str(content).lower()) else False
 
 
 def isVideo(content):
-    result = re.match(video_regex, str(content).lower())
-    return result.group(2) if result else None
+    return True if re.match(video_regex, str(content).lower()) else False
 
 
 def safeString(text):
@@ -37,8 +35,12 @@ def formatDate(dt):
 
 
 def createFolder(server_name, channel_name):
-    myDateTime = formatDate(datetime.now())
-    folder = f"{os.getcwd()}\\{safeString(server_name)}_{safeString(channel_name)}_{myDateTime}\\"
+    folder = "{}\\{}_{}_{}\\".format(
+        os.getcwd(),
+        safeString(server_name),
+        safeString(channel_name),
+        formatDate(datetime.now()),
+    )
     if not os.path.exists(folder):
         os.mkdir(folder)
     return folder
@@ -56,72 +58,146 @@ def bytes_to_mb(byte):
     return round(byte / 1024 / 1024, 3)
 
 
+async def reactForDownload(
+    command_message,
+    options,
+    options_message,
+    options_emojis,
+    images,
+    videos,
+    others,
+):
+    # Check if the user is a command author.
+    # Accept only emojis that the bot reacted.
+    # For example, if the bot reacted only '1️⃣' emoji, then the bot accept only '1️⃣' emoji.
+    def check(reaction, user):
+        return (
+            reaction.message.id == options_message.id
+            and user == command_message.author
+            and str(reaction.emoji) in options_emojis
+        )
+
+    # The bot waits for the author to choose option.
+    try:
+        reaction, r_user = await client.wait_for(
+            "reaction_add", timeout=15.0, check=check
+        )
+    except asyncio.TimeoutError:
+        await options_message.delete()
+    else:
+        # After author choose option
+        await options_message.delete()
+        selection = options[options_emojis.index(str(reaction))]
+        status_message = await command_message.channel.send(
+            f"Downloading {selection}..."
+        )
+
+        # Download Medias
+        folder = createFolder(command_message.guild.name, command_message.channel.name)
+
+        try:
+            if selection == "Images":
+                for url, name in images.items():
+                    await downloadMedia(url, folder, name)
+            elif selection == "Videos":
+                for url, name in videos.items():
+                    await downloadMedia(url, folder, name)
+            elif selection == "All":
+                for url, name in images.items():
+                    await downloadMedia(url, folder, name)
+                for url, name in videos.items():
+                    await downloadMedia(url, folder, name)
+            else:
+                print("An error occurred on selection!")
+
+        except Exception as ex:
+            print(ex)
+            await status_message.edit(content="Something went wrong!", delete_after=10)
+            return 0
+        await status_message.edit(content="Download Completed.", delete_after=10)
+
+
 class MyClient(discord.Client):
     async def on_ready(self):
         print("{0} is ALIVE!".format(self.user.name))
 
     async def on_message(self, message):
-        prefix = "`"
+        self.prefix = "`"
 
-        if message.content[: len(prefix)] == prefix:
-            command = message.content[len(prefix) :]
+        if message.content[: len(self.prefix)] == self.prefix:
+            self.command = message.content[len(self.prefix) :]
+            self.isAdmin = message.author.guild_permissions.administrator
 
             if message.author.bot:
                 print("Robots can not give me orders!")
                 return 0
 
             print(
-                f"\n{formatDate(datetime.now())}:{str(message.author)}:{command}\n{'_' * 40}\n"
+                f"\n{formatDate(datetime.now())}:{str(message.author)}:{self.command}\n{'_' * 40}\n"
             )
 
-            if command == "ping":
+            if self.command == "ping":
                 m = f"Latency: {round(self.latency * 1000, 2)}ms"
                 await message.channel.send(m)
                 print(m)
 
-            elif command.startswith("info"):
-                limit = (  # number of last sent messages
-                    int(command.split()[1])
-                    if len(command.split()) > 1 and command.split()[1].isdigit()
+            elif self.command.startswith("info"):
+                if not self.isAdmin:
+                    await message.reply("This is admin only command.", delete_after=10)
+                    print("Unauthorized")
+                    return 0
+
+                self.limit = (  # number of messages (newest to oldest)
+                    int(self.command.split()[1])
+                    if len(self.command.split()) > 1
+                    and self.command.split()[1].isdigit()
                     else 5  # default: check last 5 messages.
                 )
 
-                message_history = await message.channel.history(limit=limit).flatten()
+                self.message_history = await message.channel.history(
+                    limit=self.limit
+                ).flatten()
 
-                images, videos, others = {}, {}, {}
-                total_image_size, total_video_size, total_other_size = 0, 0, 0
+                self.images, self.videos, self.others = {}, {}, {}
+                self.total_image_size, self.total_video_size, self.total_other_size = (
+                    0,
+                    0,
+                    0,
+                )
 
-                counter = 1
-                for m in message_history:
+                self.counter = 1
+                for m in self.message_history:
                     if m.attachments and not m.author.bot:
-                        print(f"{counter} - {m.author.name}: {m.attachments[0]}")
+                        print(f"{self.counter} - {m.author.name}: {m.attachments[0]}")
 
                         if isImage(m.attachments[0]):
                             print(f"image {bytes_to_mb(m.attachments[0].size)}mb")
-                            total_image_size += m.attachments[0].size
-                            images[str(m.attachments[0])] = "{}_{}.{}".format(
+
+                            self.total_image_size += m.attachments[0].size
+                            self.images[str(m.attachments[0])] = "{}_{}.{}".format(
                                 formatDate(m.created_at),
                                 safeString(str(m.author)),
                                 str(m.attachments[0]).split(".")[-1],
                             )
                         elif isVideo(m.attachments[0]):
                             print(f"video {bytes_to_mb(m.attachments[0].size)}mb")
-                            total_video_size += m.attachments[0].size
-                            videos[str(m.attachments[0])] = "{}_{}.{}".format(
+
+                            self.total_video_size += m.attachments[0].size
+                            self.videos[str(m.attachments[0])] = "{}_{}.{}".format(
                                 formatDate(m.created_at),
                                 safeString(str(m.author)),
                                 str(m.attachments[0]).split(".")[-1],
                             )
                         else:
                             print(f"other {bytes_to_mb(m.attachments[0].size)}mb")
-                            total_other_size += m.attachments[0].size
-                            others[str(m.attachments[0])] = "{}_{}.{}".format(
+                            self.total_other_size += m.attachments[0].size
+                            self.others[str(m.attachments[0])] = "{}_{}.{}".format(
                                 formatDate(m.created_at),
                                 safeString(str(m.author)),
                                 str(m.attachments[0]).split(".")[-1],
                             )
 
-                        counter += 1
+                        self.counter += 1
 
                 # Summary report message
                 colors = [0xFF0000, 0xFFEE00, 0x40FF00, 0x00BBFF, 0xFF00BB]
@@ -129,102 +205,68 @@ class MyClient(discord.Client):
                 embedVar = discord.Embed(title="Report", color=random.choice(colors))
                 embedVar.add_field(
                     name="Messages",
-                    value=f"{len(message_history)} messages found.",
+                    value=f"{len(self.message_history)} messages found.",
                     inline=False,
                 )
                 embedVar.add_field(
                     name="Images",
-                    value=f"{len(images)} images found. (Size: {bytes_to_mb(total_image_size)}mb)"
-                    if total_image_size > 0
+                    value=f"{len(self.images)} images found. (Size: {bytes_to_mb(self.total_image_size)}mb)"
+                    if self.total_image_size > 0
                     else "No image found.",
                     inline=False,
                 )
                 embedVar.add_field(
                     name="Videos",
-                    value=f"{len(videos)} videos found. (Size: {bytes_to_mb(total_video_size)}mb)"
-                    if total_video_size > 0
+                    value=f"{len(self.videos)} videos found. (Size: {bytes_to_mb(self.total_video_size)}mb)"
+                    if self.total_video_size > 0
                     else "No video found.",
                     inline=False,
                 )
                 embedVar.add_field(
                     name="Total Size of Medias (Image/Video)",
-                    value=f"{bytes_to_mb((total_image_size + total_video_size))}mb",
+                    value=f"{bytes_to_mb((self.total_image_size + self.total_video_size))}mb",
                     inline=False,
                 )
                 await message.channel.send(embed=embedVar)
 
                 # Options (Images, Videos, All) message
                 emojis = ["1️⃣", "2️⃣", "3️⃣"]
-                options = []
+                self.options = []
 
-                if total_image_size > 0:
-                    options.append("Images")
-                if total_video_size > 0:
-                    options.append("Videos")
-                if len(options) == 2:
-                    options.append("All")
+                if self.total_image_size > 0:
+                    self.options.append("Images")
+                if self.total_video_size > 0:
+                    self.options.append("Videos")
+                if len(self.options) == 2:
+                    self.options.append("All")
 
-                if options:
-                    options_text = "__**Download Options**__"
-                    for option in options:
-                        options_text += f"\n[{options.index(option) + 1}] {option}"
+                if self.options:
+                    self.options_text = "__**Download Options**__"
+                    for option in self.options:
+                        self.options_text += (
+                            f"\n[{self.options.index(option) + 1}] {option}"
+                        )
 
-                    options_message = await message.channel.send(options_text)
+                    self.options_message = await message.channel.send(self.options_text)
 
                     # Reactions for options
-                    for i in range(len(options)):
-                        await options_message.add_reaction(emojis[i])
+                    for i in range(len(self.options)):
+                        await self.options_message.add_reaction(emojis[i])
 
-                    def check(reaction, user):
-                        # Check if the user is a command author.
-                        # Accept only emojis that the bot reacted.
-                        # For example, if the bot reacted only '1️⃣' emoji, then the bot accept only '1️⃣' emoji.
-                        return user == message.author and str(reaction.emoji) in [
-                            emojis[i] for i in range(len(options))
-                        ]
+                    self.options_emojis = [emojis[i] for i in range(len(self.options))]
 
-                    # The bot waits for the author to choose option.
-                    try:
-                        reaction, user = await client.wait_for(
-                            "reaction_add", timeout=15.0, check=check
-                        )
-                    except asyncio.TimeoutError:
-                        await options_message.delete()
-                    else:
-                        await options_message.delete()
-
-                        selection = options[emojis.index(str(reaction))]
-                        status_message = await message.channel.send(
-                            f"Downloading {selection}..."
-                        )
-
-                        # Download Medias
-                        folder = createFolder(message.guild.name, message.channel.name)
-
-                        try:
-                            if selection == "Images":
-                                for url, name in images.items():
-                                    await downloadMedia(url, folder, name)
-                            elif selection == "Videos":
-                                for url, name in videos.items():
-                                    await downloadMedia(url, folder, name)
-                            elif selection == "All":
-                                for url, name in images.items():
-                                    await downloadMedia(url, folder, name)
-                                for url, name in videos.items():
-                                    await downloadMedia(url, folder, name)
-                            else:
-                                print("An error occurred on selection!")
-
-                        except Exception as ex:
-                            print(ex)
-                            await status_message.edit(
-                                content="Something went wrong!", delete_after=10
-                            )
-                            return 0
-                        await status_message.edit(
-                            content="Download Completed.", delete_after=10
-                        )
+                    # Handles multiple commands at the same time.
+                    await reactForDownload(
+                        message,
+                        self.options,
+                        self.options_message,
+                        self.options_emojis,
+                        self.images,
+                        self.videos,
+                        self.others,
+                    )
+            else:
+                print("Unknown command")
 
 
 load_dotenv()
